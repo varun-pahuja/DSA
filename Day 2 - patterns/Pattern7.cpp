bf57c092091e112dd0bf6d7915a3478d96f4317d
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-16 09:48:58 +0000

    Sep 16, 2026, 3:18 PM

--- Day 2 - patterns/Pattern7.cpp
+++ Day 2 - patterns/Pattern7.cpp
@@ -5,15 +5,15 @@ void print1(int n){
 int i, j;
   for(i=1; i<=n; i++){
     //space
-    for(j=0; j<n-i-1; j++){  
+    for(j=0; j<=i; j++){  
         cout << " ";
     }
     //stars
-    for(j=0; j<2*i-1; j++){  
+    for(j=0; j<2*n-i+1; j++){  
        cout << "*";
     }
     //space
-    for(j=0; j<n-i-1; j++){
+    for(j=0; j<=i; j++){
        cout << " ";
     }
     cout << endl;
