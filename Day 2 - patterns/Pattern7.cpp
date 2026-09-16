c957e931ec637a6e1fa06f6b3e257c40bcc78abc
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-16 09:27:52 +0000

    Sep 16, 2026, 2:57 PM

---
+++ Day 2 - patterns/Pattern7.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print1(int n){
+int i, j;
+  for(i=1; i<=n; i++){
+    for(j=1; j<=n+i; j++){  // the sensei used j = 0; j < n-i+1; j++ but I used j = n; j >= i; j-- and it worked too
+       cout << "* ";
+    }
+    cout << endl;
+  }
+
+}
+
+int main() {
+//   int t;
+//   cin>> t;
+//   for(int i=0; i<t; i++){
+    int n;
+    cin>> n;
+    print1(n);
+  }
+  
+// } 
