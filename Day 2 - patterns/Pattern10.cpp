fe7ad573dd82dfd3e63431fec980e2b7646273db
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-16 13:11:01 +0000

    Sep 16, 2026, 6:41 PM

--- Day 2 - patterns/Pattern10.cpp
+++ Day 2 - patterns/Pattern10.cpp
@@ -5,6 +5,9 @@ void print1(int n){
 int i, j;
   for(i=1; i<=2*n-1; i++){
     int stars = i;
+    if(i>n){
+        stars = 2*n - i;
+    }
     for(j=1; j<=stars; j++){
        cout << "*";
     }
