3c4e9ae64dcca0a1cbfb354f568b9c3bf081d48b
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-16 09:53:28 +0000

    Sep 16, 2026, 3:23 PM

---
+++ Day 2 - patterns/Pattern8.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print1(int n){
+int i, j;
+  for(i=0; i<=n; i++){
+    //space
+    for(j=0; j<n-i-1; j++){  
+        cout << " ";
+    }
+    //stars
+    for(j=0; j<2*n - (2*i -1); j++){  
+       cout << "*";
+    }
+    //space
+    for(j=0; j<n-i-1; j++){
+       cout << " ";
+    }
+    cout << endl;
+  }
+
+}
+
+int main() {
+//   int t;
+//   cin>> t;
+//   for(int i=0; i<t; i++){
+    int n;
+    cin>> n;
+    print1(n);
+  }
+  
+// } 
