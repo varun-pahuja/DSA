75034dae0f9d3d3999f653332095b5092efc3554
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-16 09:14:55 +0000

    Sep 16, 2026, 2:44 PM

--- Day 2 - patterns/Pattern6.cpp
+++ Day 2 - patterns/Pattern6.cpp
@@ -5,7 +5,7 @@ void print1(int n){
 int i, j;
   for(i=1; i<=n; i++){
     for(j=n; j>=i; j--){  // the sensei used j = 0; j < n-i+1; j++ but I used j = n; j >= i; j-- and it worked too
-       cout << i;
+       cout << n-i+1;
     }
     cout << endl;
   }
