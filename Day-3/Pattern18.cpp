c16fcd7004c735c1016c8ac003da16f3acb9a2bc
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-19 18:11:55 +0000

    Sep 19, 2026, 11:41 PM

---
+++ Day-3/Pattern18.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print1(int n){
+int i, j;
+int num = 1;
+  for(i=0; i<=n; i++){
+    char ch = 'A'-i;
+    for(j=0; j<=i; j++){
+      cout << ch << " ";
+    }
+    cout << endl;
+  }
+
+}
+
+int main() {
+//   int t;
+//   cin>> t;
+//   for(int i=0; i<t; i++){
+    int n;
+    cin>> n;
+    print1(n);
+  }
+  
+// } 
