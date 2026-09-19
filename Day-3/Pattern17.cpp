e4ce7fbf1c89a44600921144b12d956567338368
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-19 15:19:05 +0000

    Sep 19, 2026, 8:49 PM

--- Day-3/Pattern17.cpp
+++ Day-3/Pattern17.cpp
@@ -4,30 +4,25 @@ using namespace std;
 void print1(int n){
     int i, j;
     for(i=1; i<=n; i++){
-        for(j=0; j<n-i; j++)       // ✅ fix 1
+        for(j=0; j<n-i; j++)
             cout << " ";
 
         char ch = 'A';
-        int breakpoint = i+1;       // ✅ fix 2
+        int breakpoint = i+1;
         for(j=1; j<=2*i-1; j++){
             cout << ch;
-            if(j <= breakpoint) ch++;
+            if(j < breakpoint) ch++;
             else ch--;
         }
 
-        for(j=0; j<n-i; j++)       // ✅ fix 1
+        for(j=0; j<n-i; j++)
             cout << " ";
         cout << endl;
     }
 }
 
 int main() {
-//   int t;
-//   cin>> t;
-//   for(int i=0; i<t; i++){
     int n;
-    cin>> n;
+    cin >> n;
     print1(n);
-  }
-  
-// } 
+}
