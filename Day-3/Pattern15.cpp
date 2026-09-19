868975bcda5a34ecd3b0315a995ae24795dbd159
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-19 15:00:11 +0000

    Sep 19, 2026, 8:30 PM

--- Day-3/Pattern15.cpp
+++ Day-3/Pattern15.cpp
@@ -5,7 +5,7 @@ void print1(int n){
 int i, j;
 int num = 1;
   for(i=0; i<=n; i++){
-    for( char ch = 'A'; ch <= 'A' - i + 1; ch++){
+    for( char ch = 'A'; ch <= 'A' + i - 1; ch++){
       cout << ch << " ";
     }
     cout << endl;
