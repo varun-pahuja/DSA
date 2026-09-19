b0d900da81384988d51119bf57e0975f161cc368
varun-pahuja <varunpahuja2005@gmail.com>
2026-09-19 18:37:41 +0000

    Sep 20, 2026, 12:07 AM

--- Day-3/Pattern20.cpp
+++ Day-3/Pattern20.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 void print1(int n){
 int i, j;
+int spaces = 2*n - 2;
   for(i=1; i<=2*n-1; i++){
     int stars = i;
     if(i>n){
@@ -13,14 +14,21 @@ int i, j;
        cout << "*";
     }
     //space
-
+    for(j=1; j<= spaces; j++){
+       cout << " ";
+    }
     //stars
     for(j=1; j<=stars; j++){
        cout << "*";
     }
     cout << endl;
+    if (i<n){
+        spaces -= 2;
   }
-    
+    else{
+        spaces += 2;
+    }
+}
 }
 
 int main() {
